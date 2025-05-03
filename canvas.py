6a4206dfc80f9a04509e760c7b153a6a9ef0ed83
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-05-04 00:23:07 +0300

    another GUI idea(vertex mover in a separate window)

--- canvas.py
+++ canvas.py
@@ -3,4 +3,15 @@ class Canvas(tk.Canvas):
     def __init__(self, root):
         super().__init__(root,  bg="#FFFFFF")
         self.place(relx=0.01, rely=0.01,relwidth=0.8, relheight=0.9)
-                
+        self.__zoom =30
+
+    @property
+    def zoom(self):
+        return self.__zoom
+
+    @zoom.setter
+    def zoom(self, value):
+        self.__zoom = value
+
+    def redraw(self, *args):
+        pass
